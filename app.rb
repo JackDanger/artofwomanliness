40fdca7669da724ac6bd4d82b634ad7e10fbf470
Jack Danger Canty <gitcommit@6brand.com>
2010-10-12 21:18:47 -0700

    cleaner analytics

--- app.rb
+++ app.rb
@@ -134,16 +134,5 @@ def add_custom_logo html
 end
 
 def tag_with_analytics html
-  html.sub '</body>', ANALYTICS+'</body>'
+  html.sub 'UA-1066823-4', 'UA-331450-15'
 end
-ANALYTICS = %q{
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-331450-15']);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>}
